2b2124e5b1e4ea121a150690d1058247e745089e
merge-simpson <letsdev@kakao.com>
2023-05-17 21:15:46 +0900

    fix(member validation): allow eng for name. add 최대 N 글자 onto validation message

--- src/main/java/com/example/demo/members/api/constants/MemberValidationConstants.java
+++ src/main/java/com/example/demo/members/api/constants/MemberValidationConstants.java
@@ -4,14 +4,14 @@ package com.example.demo.members.api.constants;
 public final class MemberValidationConstants {
     public static final class UsernameValidation {
         public static final String PATTERN = "^[a-zA-Z][a-zA-Z0-9]{2,30}$";
-        public static final String MESSAGE = "계정은 영문으로 시작하여야 하며, 영문, 숫자를 포함하여 최소 3글자 이상 작성하시기 바랍니다.";
+        public static final String MESSAGE = "계정은 영문으로 시작하여야 하며, 영문, 숫자를 포함하여 최소 3글자 이상 작성하시기 바랍니다. (최대 30 글자)";
 
         private UsernameValidation() {}
     }
 
     public static final class NameValidation {
-        public static final String PATTERN = "^[가-힣]{2,50}$";
-        public static final String MESSAGE ="이름은 완성된 한글만 허용하며, 2글자 이상 입력하시기 바랍니다.";
+        public static final String PATTERN = "^[가-힣a-zA-Z]{2,50}$";
+        public static final String MESSAGE ="이름은 완성된 한글과 영문만 허용하며, 2글자 이상 입력하시기 바랍니다. (최대 50 글자)";
 
         private NameValidation() {}
     }
