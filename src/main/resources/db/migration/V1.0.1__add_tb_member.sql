216582d81b5624739a207a499df6b94a1d30636f
merge-simpson <letsdev@kakao.com>
2024-04-18 22:54:19 +0900

    fix(member, flyway): remove autoincrement

--- src/main/resources/db/migration/V1.0.1__add_tb_member.sql
+++ src/main/resources/db/migration/V1.0.1__add_tb_member.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS member (
-    id              BINARY(16)          AUTO_INCREMENT              PRIMARY KEY,
+    id              BINARY(16)                                      PRIMARY KEY,
     username        VARCHAR(255)        NOT NULL                                        COMMENT '계정',
     password        VARCHAR(255)                                                        COMMENT '비밀번호',
     full_name       VARCHAR(255)                                                        COMMENT '본명(full name)',
