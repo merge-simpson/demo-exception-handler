285f2b35f7102773857e5c93bbd1624a10d2c7a3
merge-simpson <letsdev@kakao.com>
2024-04-18 21:59:15 +0900

    refactor: use new ApiResponseError, ApiSimpleError in GlobalExceptionHandler

--- src/main/java/com/example/demo/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/common/exception/GlobalExceptionHandler.java
@@ -1,52 +1,56 @@
 package com.example.demo.common.exception;
 
 import com.example.demo.common.exception.status2xx.NoContentException;
-import com.example.demo.common.support.exception.ApiError;
-import com.example.demo.common.support.exception.ApiError.ApiSubError;
 import com.example.demo.common.support.exception.CustomException;
-import com.example.demo.common.support.exception.ErrorCode;
+import com.example.demo.common.support.exception.response.ApiResponseError;
+import com.example.demo.common.support.exception.response.ApiSimpleError;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.time.Instant;
 
 @RestControllerAdvice
 public final class GlobalExceptionHandler {
     // 따로 더 구체적인 선언이 없다면, CustomException을 상속받은 모든 예외가 이곳으로 온다. 따라서 이 양식을 따르는 한 따로 더 만들 익셉션 핸들러 메서드가 없다.
     @ExceptionHandler(CustomException.class)
-    public ResponseEntity<ApiError> handleMemberException(CustomException exception) {
-        ErrorCode errorCode = exception.getErrorCode();
-        HttpStatus httpStatus = errorCode.defaultHttpStatus();
-        ApiError response = ApiError.of(errorCode, getPlattedApiSubErrors(exception.getCause()));
+    public ResponseEntity<ApiResponseError> handleMemberException(CustomException exception) {
+        ApiResponseError response = ApiResponseError.of(exception);
+        HttpStatus httpStatus = exception
+                .getErrorCode()
+                .defaultHttpStatus();
 
         return new ResponseEntity<>(response, httpStatus);
+    }
+
+    /**
+     * validation 애노테이션(유효성) 예외를 최종적으로 이곳에서 처리할 수 있음.
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponseError> handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException exception) {
+        FieldError fieldError = exception.getBindingResult().getFieldError();
+
+        ApiResponseError response = ApiResponseError.builder()
+                .name(exception.getBody().getTitle())
+                .message(
+                        fieldError == null ? exception.getMessage() : fieldError.getDefaultMessage()
+                )
+                .status(exception.getStatusCode().value())
+                .timestamp(Instant.now())
+                .cause(ApiSimpleError.listOfCauseSimpleError(exception.getCause()))
+                .build();
+
+        return ResponseEntity
+                .status(exception.getStatusCode())
+                .body(response);
     }
 
     @ExceptionHandler(NoContentException.class)
     public ResponseEntity<?> handleNoContentException(NoContentException exception) {
         return ResponseEntity.noContent().build();
-    }
-
-    private ApiSubError[] getPlattedApiSubErrors(Throwable cause) {
-        Throwable currentCause = cause;
-        List<ApiSubError> subErrors = new ArrayList<>();
-
-        while (currentCause != null) {
-            String errorFullName = currentCause.getClass().getSimpleName();
-            String field = errorFullName.substring(errorFullName.lastIndexOf('.') + 1);
-            subErrors.add(
-                    ApiSubError.builder()
-                            .field(field)
-                            .message(currentCause.getLocalizedMessage())
-                            .build()
-            );
-            currentCause = currentCause.getCause();
-        }
-
-        ApiSubError[] subErrorArray = new ApiSubError[subErrors.size()];
-        return subErrors.toArray(subErrorArray);
     }
 }
