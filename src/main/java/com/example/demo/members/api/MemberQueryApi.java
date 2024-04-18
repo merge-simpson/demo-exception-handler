ec0d4baa5cbb8998ff9702b877b70bb9428544ef
merge-simpson <letsdev@kakao.com>
2024-04-18 22:04:47 +0900

    chore(members): add comment that Page<...> is not suggested

--- src/main/java/com/example/demo/members/api/MemberQueryApi.java
+++ src/main/java/com/example/demo/members/api/MemberQueryApi.java
@@ -20,11 +20,13 @@ public final class MemberQueryApi {
 
     @GetMapping("/members")
     public MemberQueryAllResponseDto all(
-            @PageableDefault(size=12, sort="joinDate", direction = Sort.Direction.DESC) Pageable pageable) {
+            @PageableDefault(size=12, sort="joinDate", direction = Sort.Direction.DESC) Pageable pageable
+    ) {
         // Page 관련 전처리
         // Zero Based Paging to One Based Paging(External)
         pageable = pageable.previousOrFirst(); // 0 이하는 모두 1 페이지.
 
+        // Page<...> 사용 시 성능 저하가 발생하므로 삼갈 것. 여기서는 예시를 위해 작성함.
         Page<Member> memberPage = memberSearchUseCase.findAll(pageable);
 
         if (memberPage.isEmpty()) throw new NoContentException();
