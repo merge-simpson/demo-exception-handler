5b36c66ea0a9298884a5260fdfb6517563f186ba
merge-simpson <letsdev@kakao.com>
2024-04-18 23:06:50 +0900

    refactor(member): reorder member error code methods to meet the interface

--- src/main/java/com/example/demo/members/exception/MemberErrorCode.java
+++ src/main/java/com/example/demo/members/exception/MemberErrorCode.java
@@ -8,21 +8,23 @@ import org.springframework.http.HttpStatus;
 public enum MemberErrorCode implements ErrorCode {
     USERNAME_ALREADY_EXISTS("이미 사용 중인 계정입니다.", HttpStatus.CONFLICT),
     SIGN_UP_FAILED_DEFAULT(
-            "회원 가입을 다시 진행해 주십시오. 오류가 지속되는 경우 문의하시기 바랍니다.", HttpStatus.INTERNAL_SERVER_ERROR),
+            "회원 가입을 다시 진행해 주십시오. 오류가 지속되는 경우 문의하시기 바랍니다.",
+            HttpStatus.INTERNAL_SERVER_ERROR
+    ),
     MEMBER_NOT_FOUND("회원을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
     DEFAULT("회원 취급 중 오류가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
 
-    private final String MESSAGE;
-    private final HttpStatus STATUS;
-
-    @Override
-    public HttpStatus defaultHttpStatus() {
-        return STATUS;
-    }
+    private final String message;
+    private final HttpStatus status;
 
     @Override
     public String defaultMessage() {
-        return MESSAGE;
+        return message;
+    }
+
+    @Override
+    public HttpStatus defaultHttpStatus() {
+        return status;
     }
 
     @Override
