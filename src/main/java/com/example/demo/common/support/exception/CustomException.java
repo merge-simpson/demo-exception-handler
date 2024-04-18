98540128a2dbb2bd612da5de5d4c96f2dda30a27
merge-simpson <letsdev@kakao.com>
2024-04-18 21:45:21 +0900

    refactor: reorder code lines to meet the Google convention

--- src/main/java/com/example/demo/common/support/exception/CustomException.java
+++ src/main/java/com/example/demo/common/support/exception/CustomException.java
@@ -4,41 +4,15 @@ import org.springframework.http.HttpStatus;
 
 public class CustomException extends RuntimeException {
 
-    protected ErrorCode ERROR_CODE;
-
+    // ===== statics =====
     private static ErrorCode getDefaultErrorCode() {
         return DefaultErrorCodeHolder.DEFAULT_ERROR_CODE;
     }
 
-    private static class DefaultErrorCodeHolder {
-        private static final ErrorCode DEFAULT_ERROR_CODE = new ErrorCode() {
-            @Override
-            public String name() {
-                return "SERVER_ERROR";
-            }
+    // ===== non-static fields =====
+    protected ErrorCode ERROR_CODE;
 
-            @Override
-            public HttpStatus defaultHttpStatus() {
-                return HttpStatus.INTERNAL_SERVER_ERROR;
-            }
-
-            @Override
-            public String defaultMessage() {
-                return "서버 오류";
-            }
-
-            @Override
-            public RuntimeException defaultException() {
-                return new CustomException("SERVER_ERROR");
-            }
-
-            @Override
-            public RuntimeException defaultException(Throwable cause) {
-                return new CustomException("SERVER_ERROR", cause);
-            }
-        };
-    }
-
+    // ===== Constructors =====
     public CustomException() {
         this.ERROR_CODE = getDefaultErrorCode();
     }
@@ -63,7 +37,38 @@ public class CustomException extends RuntimeException {
         this.ERROR_CODE = errorCode;
     }
 
+    // ===== Non-static Methods =====
     public ErrorCode getErrorCode() {
         return ERROR_CODE;
+    }
+
+    // ===== Inner Classes =====
+    private static class DefaultErrorCodeHolder { // 사용할 때 로드 + 스레드 세이프(클래스 로드 타임은 동시성 보장됨.)
+        private static final ErrorCode DEFAULT_ERROR_CODE = new ErrorCode() {
+            @Override
+            public String name() {
+                return "SERVER_ERROR";
+            }
+
+            @Override
+            public HttpStatus defaultHttpStatus() {
+                return HttpStatus.INTERNAL_SERVER_ERROR;
+            }
+
+            @Override
+            public String defaultMessage() {
+                return "서버 오류";
+            }
+
+            @Override
+            public RuntimeException defaultException() {
+                return new CustomException(this);
+            }
+
+            @Override
+            public RuntimeException defaultException(Throwable cause) {
+                return new CustomException(this, cause);
+            }
+        };
     }
 }
--- src/main/java/com/example/demo/common/support/exception/ErrorCode.java
+++ src/main/java/com/example/demo/common/support/exception/ErrorCode.java
@@ -3,9 +3,9 @@ package com.example.demo.common.support.exception;
 import org.springframework.http.HttpStatus;
 
 public interface ErrorCode {
-    String name();
-    HttpStatus defaultHttpStatus();
+    String name(); // automatically overridden in enum
     String defaultMessage();
+    HttpStatus defaultHttpStatus();
     RuntimeException defaultException();
     RuntimeException defaultException(Throwable cause);
 }
