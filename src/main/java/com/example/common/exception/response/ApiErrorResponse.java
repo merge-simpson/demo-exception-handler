887d5ef7730dd3c45898ec708168a1dda69d4b65
merge-simpson <letsdev@kakao.com>
2025-03-01 03:05:24 +0900

    chore(exception handler): add description to the class documentation comment of `ApiErrorResponse`

--- src/main/java/com/example/common/exception/response/ApiErrorResponse.java
+++ src/main/java/com/example/common/exception/response/ApiErrorResponse.java
@@ -12,12 +12,26 @@ import java.util.Map;
 
 /**
  *
+ * 실제 프로젝트에 적용 시 하기 항목 중 필요한 것만 고르는 것도 편리한 구현 전략 중 하나입니다.
+ * <br />
+ *
+ * <blockquote>
+ *     <p>하기 항목을 고려해 각 프로젝트에서 원하는 수준으로 리팩토링 하세요.</p>
+ *     <ul>
+ *         <li>
+ *             {@code @JsonInclude(...)}는 이 영역에 Jackson 의존성을 침투시킵니다.
+ *             라이브러리 모듈화 시 Jackson 의존성을 {@code compileOnly("...")}로 제공하면
+ *             consumer 모듈에서 이 의존성을 회피할 수 있습니다.
+ *         </li>
+ *     </ul>
+ * </blockquote>
+ *
  * @param code 에러 코드 명
  * @param status 상태 코드 값
  * @param name 오류 이름
  * @param message 오류 메시지
  * @param error HTTP 상태 코드 이름
- * @param cause
+ * @param cause 이 오류를 야기한 오류
  * @param timestamp 발생 시각
  * @param path 오류가 발생한 경로
  * @param payload extension members
